b3247dd52b464efd0bcfbac9d51cbd01f6501124
Ricardo Georgel <rickygeorgel@yahoo.com.br>
2022-05-09 12:07:36 -0700

    feat: add create and update functions to graphql

--- src/infrastructure/http/graphql/resolvers/shortUrl.resolver.ts
+++ src/infrastructure/http/graphql/resolvers/shortUrl.resolver.ts
@@ -1,5 +1,6 @@
-import { Arg, Query, Resolver } from 'type-graphql';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
 import { ShortUrl } from '@app/infrastructure/http/graphql/models/shortUrl.graphql';
+import { ShortUrl as ShortUrlModel } from '@app/application/models/index';
 import shortUrlService from '@app/application/services/shortUrl.service';
 import { ShortUrlAdapter } from '@app/adapters/http/graphql/shortUrl.adapter';
 
@@ -19,6 +20,29 @@ export default class {
   ): Promise<ShortUrl | undefined> {
     const shortUrl = await shortUrlService.getByCode(code);
     return ShortUrlAdapter.toGraphql(shortUrl);
+  }
+
+  @Mutation(() => ShortUrl)
+  async createShortUrl(
+    @Arg('originalUrl') originalUrl: string,
+  ): Promise<ShortUrl> {
+    const shortUrl = await shortUrlService.create(originalUrl);
+    return ShortUrlAdapter.toGraphql(shortUrl)!;
+  }
+
+  @Mutation(() => ShortUrl)
+  async updateShortUrl(
+    @Arg('originalUrl') originalUrl: string,
+    @Arg('code') code: string,
+    @Arg('shortUrlId') shortUrlId: string,
+  ): Promise<ShortUrl> {
+    const shortUrl = new ShortUrlModel({
+      originalUrl,
+      code,
+      shortUrlId
+    });
+    const response = await shortUrlService.update(shortUrl);
+    return ShortUrlAdapter.toGraphql(response)!;
   }
 
 }
